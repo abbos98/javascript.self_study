188e84457d3074aa5eeabda21f675e99b5bae9ac
Abbos <abbos.pankiy2018@gmail.com>
2024-07-21 17:00:11 +0900

    Type covertion vs Logical operators

---
+++ lesson3/script.js
@@ -0,0 +1,67 @@
+// cd ..   vs   cd (name of folder)
+
+// << Hoisting >>
+
+
+// var
+
+// console.log(num);
+// var num = 100;
+
+// => var hoisting boladi hamda qiymatini underfiend deb qaytaradi! (only decloration appears)
+
+
+
+// let 
+
+// console.log(num);
+// let num = 100
+
+// => let hoisting bo'lmaydi
+
+
+
+// const
+
+// console.log(num);
+// let num = 100;
+
+// => const hoisting bo'lmaydi
+
+
+///////////////////////////////////////////////////////////////////////////////////
+
+// Type convertion
+
+// 1) Implicit (avto)
+// 2) Explisit (mutual)
+
+
+// Truthy and falsy values 
+
+// Truthy => !0, !null, !underfiend
+
+// Falsy => 0, null, underfiend
+
+///////////////////////////////////////////////////////////////////////////////////////
+
+
+// << Logical operators >>
+
+
+// && => logical and (all have to be true)
+
+// let car = 'Bugatti';
+
+// car == 'Bugatti' && console.log('$1.000.000');
+
+
+// || => logical or (even one be true result will be true)
+
+// let num = 4 == 10 || 5 == 5 || 6 == 9;
+// console.log(num);
+
+
+// Logical not
+
+// trueni falsega falseni truega aylantirib beradi
