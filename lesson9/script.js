8d21d078b54423f315a022d71ddce50bbf3da1c8
Abbos <abbos.pankiy2018@gmail.com>
2024-07-21 17:13:08 +0900

    Object vs Object methods

---
+++ lesson9/script.js
@@ -0,0 +1,116 @@
+// Object 
+
+// let user = {
+//     name:'Abbos',
+//     age: 20
+// }
+
+
+// Accessing to object value 
+
+// <<<<<<< 1 >>>>>>>>
+
+// let student = {
+//     name: 'Abbos',
+//     age:20,
+//     'Full-name': 'Begmatov Abbos Abduzokhid Ugli',
+//     100: 2200,
+// }
+//<<<<<<<<<< 1-usul >>>>>>>>>>
+
+// console.log(student.name);
+// console.log(student.age);                  
+// console.log(student.Full-name);     
+// console.log(student.100);           
+
+// <<<<<<<<< 2-usul >>>>>>>>>>
+// console.log(student['name']);
+// console.log(student['age']);              
+// console.log(student['Full-name']);
+// console.log(student[100]);
+
+
+// <<<<<<<<< 3-usul (dynamic access) >>>>>>>>>
+
+// let student = {
+//     name: 'Abbos',
+//     age:20,
+// }
+
+// let key = 'name';
+
+// console.log(student[key]);
+
+
+//////////////////////////////////////////////////////////////////////////
+
+// Object update, delete, create
+
+// let student = {
+//         name: 'Madina',
+//         age:20,
+//         status: 'Student',
+//     }
+
+//     // Object.freeze(student);
+//     // Object.seal(student);
+
+
+//    student.name = 'Abbosbek';       
+//    student.status = 'Student';       
+//    delete student.age
+
+//    console.log(student);
+
+//////////////////////////////////////////////////////////////
+
+// <<<<<< Object methods >>>>>>>>>
+
+// let student = {
+//     name: 'Abbos',
+//     age:20,
+// }
+
+// console.log(Object.keys(student));
+// console.log(Object.values(student));
+// console.log(Object.entries(student));
+
+/////////////////////////////////////////////////////////////
+
+// <<<<<<< For in loop >>>>>>>>
+
+// let student = {
+//         name: 'Abbos',
+//         age:20,
+//     }
+    
+//     for(let key in student) {
+//         // console.log(key);
+//         // console.log(student[key]);
+//     }
+
+/////////////////////////////////////////////////////////////////////
+
+// let obj = {
+//     first: 1,
+//     second: 3,
+//     third: 5
+// }
+
+// console.log(Object.values(obj).sort());
+
+
+
+// What is function ?
+// Declaration vs Expression vs arrow functions ?
+// Defult parametr in functions ?
+// Hoisting in functions ?
+
+// What is object ?
+// Why object is non-primitive ?
+// How to access object ?
+// Difference between . and [""] ?
+// What is dynamic access to object value ?
+// Object.freeze() vs Object.seal() ?
+// Object.keys() vs Object.values() vs Object.entries()?
+// What is for in loop ?
