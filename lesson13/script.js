0092283995bb205451f1afe922c46f88438d46bf
Abbos <abbos.pankiy2018@gmail.com>
2024-07-25 02:57:19 +0900

    prototype

--- lesson13/script.js
+++ lesson13/script.js
@@ -1,20 +1,20 @@
 //  <<< Prototype >>>
 
 
-const user = {
-    name: 'Abbos',
-    lastName: 'Begmatov',
-    age: 25,
+// const user = {
+//     name: 'Abbos',
+//     lastName: 'Begmatov',
+//     age: 25,
 
 
-greet() {
-    console.log('Hi');
- }
-}
+// greet() {
+//     console.log('Hi');
+//  }
+// }
 
-Object.prototype.getAge = function (a, b) {         //global object prototypega asoslanib function yaratish!
-    return a + b
-}
+// Object.prototype.getAge = function (a, b) {         //global object prototypega asoslanib function yaratish!
+//     return a + b
+// }
 
 // Array.prototype.getName = function (name) {             //arrayga asoslanib function yaratish bu global bo'lmapydi!
 //     console.log(`Hi ${name}`);
@@ -29,8 +29,8 @@ Object.prototype.getAge = function (a, b) {         //global object prototypega 
 // let num = new Number(100);                      // agar shu holatni yozmasa inspectda prototype chiqmaydi
 
 
-String.prototype.sayHi = function name() {
-    return `Hi ${name}`
-}
-// 
-let str = new String('WBA');
+// String.prototype.sayHi = function name() {
+//     return `Hi ${name}`
+// }
+// // 
+// let str = new String('WBA');
