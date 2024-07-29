4388dec64bab4b480dbac756873d441515f76e98
Abbos <abbos.pankiy2018@gmail.com>
2024-07-30 05:32:24 +0900

    class

--- lesson15/script.js
+++ lesson15/script.js
@@ -85,3 +85,49 @@
 // let pr = new car3('Tesla','G-3','white','250 p/h','expensive')
 // pr.clickIt();
 
+
+
+
+
+
+
+
+
+
+
+
+
+// Function evalNumbers() prints the result after evaluating arithmetic operations between two numbers of addition, multiplication, division, and modulus in JavaScript.
+
+//  1 Print result of num1+num2 if operation is “add”
+//  2 Print result of num1-num2 if operation is “subtract”
+//  3 Print result of num1*num2 if operation is “multiply”
+//  4 Print result of num1/num2 if operation is “divide”
+//  5 Print result of num1%num2 if operation is “modulus”
+//  6 Else print “Invalid operation”
+
+
+// function evelNum(num1, num2, operation) {
+
+//     if(operation == 'add') {
+//         console.log(num1 + num2);
+//     } else if(operation == 'subtract') {
+//         console.log(num1 - num2);
+//     } else if( operation == 'multiply') {
+//         console.log(num1 * num2);
+//     } else if(operation == 'divide') {
+//         console.log(num1 / num2);
+//     } else if (operation === 'modules') {
+//         console.log(Math.abs(num1 - num2));
+//     } else {
+//         console.log('Invalid operation');
+//     } 
+// }
+// evelNum(3, 5, 'add');
+// evelNum(3, 5, 'subtract');
+// evelNum(3, 5, 'multiply');
+// evelNum(3, 5, 'divide');
+// evelNum(3, 5, 'modules');
+
+
+
