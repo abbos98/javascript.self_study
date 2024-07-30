d5fe4070b16e8a996b76a26fb0f0a51bf75dbd8a
Abbos <abbos.pankiy2018@gmail.com>
2024-07-30 11:02:03 +0900

    Error handling vs Synchronous, Asynchronous

---
+++ lesson16/script.js
@@ -0,0 +1,56 @@
+// <<< Error handling >>>
+
+// // let add = (a, b) => {
+// //     return a+ b
+// // }
+
+// try{
+//     let result = add(10, 20);
+//     console.log(result);
+// } catch (error) {
+//     console.log(error.name);
+//     console.log(error.message);
+// }
+// =======================================================================================
+
+// Asynchrous javascript
+
+// => Synchronous javascript
+
+// function A() {
+//     console.log('A');
+// }
+
+// function B() {
+//     console.log('B');
+// }
+
+// A();
+// B();
+
+
+// ===================================================================
+
+// Asynchronous Javascript
+
+// console.log('Abbos1');
+
+// setTimeout(() => {
+//     console.log('Abbos2');
+// }, 2000)
+
+
+// console.log('Abbos3');
+
+// ====================
+
+// setInterval();
+
+// setInterval(() => {
+//     let d = new Date();
+//     console.log(`${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`);
+// }, 1000);
+
+
+
+// plashetda yaxshiroq tuwunish uchun rasm ham bor!!!!
